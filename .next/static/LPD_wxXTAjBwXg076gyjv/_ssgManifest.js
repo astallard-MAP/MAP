86d6993f43e686b7a1d936b5bbdfd826d01763eb
Andrew Stallard <andrew@auctiondepartment.com>
2026-03-23 12:18:08 +0000

    Cleanup: Final removal of build artifacts from repository

--- .next/static/LPD_wxXTAjBwXg076gyjv/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fdocument-management\u002Feditor\u002F[id]","\u002Fdashboard\u002Fproperties\u002F[id]","\u002Fdashboard\u002Fsolicitor\u002Feditor\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
