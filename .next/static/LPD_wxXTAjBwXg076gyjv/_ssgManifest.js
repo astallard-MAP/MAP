6462b74b5572aba8229de1164dd273c2eb07fd2d
Andrew Stallard <andrew@auctiondepartment.com>
2026-03-23 12:52:12 +0000

    UI: Replaced corrupted logo and assets in public folder

---
+++ .next/static/LPD_wxXTAjBwXg076gyjv/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fdocument-management\u002Feditor\u002F[id]","\u002Fdashboard\u002Fproperties\u002F[id]","\u002Fdashboard\u002Fsolicitor\u002Feditor\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
